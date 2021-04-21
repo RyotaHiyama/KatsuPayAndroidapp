dd0d6258458d7bfeebfada85ba16507ddf245538
katsupay <katsupay.hcu@gmail.com>
2021-04-21 11:47:04 +0900

    リファクタリング(JsonParser.kt)、使用していないメソッド(parse)の削除、if文を整理

--- app/src/main/java/com/sample/katsupay/data/JsonParser.kt
+++ app/src/main/java/com/sample/katsupay/data/JsonParser.kt
@@ -1,34 +1,19 @@
 package com.sample.katsupay.data
 
-import android.util.Log
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.sample.katsupay.communication.data.Customer
 import com.sample.katsupay.communication.data.Product
 import com.sample.katsupay.communication.data.Store
 import com.sample.katsupay.communication.data.Transaction
-import org.json.JSONException
-import org.json.JSONObject
 
 object JsonParser {
-    fun parse(str:String, query:String) : String? {
-        var value = ""
-        try {
-            val jsonObjRoot = JSONObject(str)
-            value = jsonObjRoot.getString(query)
-        } catch (e:JSONException) {
-            Log.e("ERROR", "NOT FOUND VALUE")
-            return null
-        }
-        return value
-    }
-
     fun transactionParse(str:String) : List<Transaction>? {
-        if(str == "") return null // 何も文字列が含まれていない
+        return if(str == "") null // 何も文字列が含まれていない
         else {
             val mapper = jacksonObjectMapper()
             val jObjList:List<Transaction> = mapper.readValue(str)
-            return jObjList
+            jObjList
         }
     }
 
