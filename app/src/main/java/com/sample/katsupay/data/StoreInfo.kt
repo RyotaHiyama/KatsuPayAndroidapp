74b5d5138011c9948f5b291b4e0064ece137a747
katsupay <katsupay.hcu@gmail.com>
2021-04-21 11:02:40 +0900

    リファクタリング(StoreInfo.kt)、定義をclassからobjectに変更

--- app/src/main/java/com/sample/katsupay/data/StoreInfo.kt
+++ app/src/main/java/com/sample/katsupay/data/StoreInfo.kt
@@ -2,11 +2,9 @@ package com.sample.katsupay.data
 
 import com.sample.katsupay.activity.Password
 
-open class StoreInfo {
-    companion object {
+object StoreInfo {
         var storeId: String? = null
         private var password: Password? = null
         var email: String? = null
         var birthday: String? = null
-    }
 }
