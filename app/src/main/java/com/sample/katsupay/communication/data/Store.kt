f3f1fa18b7ec2117b44bd29022a6a8a5f4e2bebc
katsupay <katsupay.hcu@gmail.com>
2021-04-21 11:30:09 +0900

    リファクタリング(Store.kt)、varの位置を整える

--- app/src/main/java/com/sample/katsupay/communication/data/Store.kt
+++ app/src/main/java/com/sample/katsupay/communication/data/Store.kt
@@ -3,7 +3,7 @@ package com.sample.katsupay.communication.data
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Store (
-    @JsonProperty("storeId")  var storeId:String,
+    @JsonProperty("storeId")     var storeId:String,
     @JsonProperty("password")    var password:String?,
     @JsonProperty("email")       var email:String?,
     @JsonProperty("birthday")    var birthday:String?
