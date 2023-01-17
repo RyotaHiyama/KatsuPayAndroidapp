d4d9a07c218ae9fe8ab8c7d0f069b2850b7f7e04
RyotaHiyama <ryotahiyama@ca.info.hiroshima-cu.ac.jp>
2023-01-17 18:03:34 +0900

    購入履歴用のデータ

---
+++ app/src/main/java/com/sample/katsupay/data/TransactionInfo.kt
@@ -0,0 +1,10 @@
+package com.sample.katsupay.data
+
+object TransactionInfo {
+    var customer_id: String? = null
+    var store_id: String? = null
+    var time = ""
+    var price = ""
+    var products: Array<String> = arrayOf("")
+    var isCharge = ""
+}
